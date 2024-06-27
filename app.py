cf40c654c0dce13dfa589bae1656bb55a739e45f
juniperus01 <jiyabhagat402@gmail.com>
2024-06-27 11:37:51 +0530

    Implemented login and register routes for User authentication

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_jwt_extended import JWTManager, create_access_token
 from pymongo import MongoClient
-from auth import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timezone
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SECRET_KEY'] = 'fca5b60df8d349cb9f5828b9531cadd0'
+jwt = JWTManager(app)
 
-# Initialize MongoClient and Database
+# Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-db = client.ToDo_database
+db = client.myDatabase
 
 
 # Home route
@@ -16,5 +19,30 @@ def home():
     return jsonify(message="Welcome to ToDo app!")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# User registration route
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    if db.users.find_one({"email": data['email']}):
+        return jsonify(message="User already exists"), 400
+    hashed_password = generate_password_hash(data['password'])
+    user = {
+        "email": data['email'],
+        "password": hashed_password,
+        "created_at": datetime.now(timezone.utc)
+    }
+    db.users.insert_one(user)
+    return jsonify(message="User registered successfully"), 201
+
+
+# User login route
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = db.users.find_one({"email": data['email']})
+    
+    if user and check_password_hash(user["password"], data["password"]):
+        access_token = create_access_token(identity=data['email'])
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify(message="Invalid credentials"), 401
--- auth.py
+++
@@ -1,6 +0,0 @@
-from flask_jwt_extended import JWTManager
-from flask import Blueprint
-
-jwt = JWTManager()
-
-auth_bp = Blueprint('auth', __name__)
